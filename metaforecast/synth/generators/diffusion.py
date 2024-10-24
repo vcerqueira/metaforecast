3b1fe287d8b1bf00dc484358a85e9763e16174c1
André Catarino <andre.almeida.catarino@gmail.com>
2024-10-25 00:18:03 +0100

    chore: script removal

--- metaforecast/synth/generators/diffusion.py
+++ metaforecast/synth/generators/diffusion.py
@@ -260,27 +260,3 @@ class DiffusionGenerator(SemiSyntheticGenerator):
         x = x - predicted_noise
         return pd.DataFrame({'ds': df['ds'], 'y': x})
     
-
-if __name__ == '__main__':
-    df = pd.DataFrame({
-        'unique_id': ['A'] * 10,
-        'ds': pd.date_range(start='2020-01-01',
-                            periods=10,
-                            freq='D'),
-        'y': np.random.normal(0, 1, 10)
-    })
-
-    diffusion = GaussianDiffusion(sigma=0.2, knot=4, rename_uids=True)
-    synth_df = diffusion.transform(df, n_series=1)
-
-    assert synth_df.shape[0] == 10
-    assert synth_df['unique_id'].nunique() == 1
-
-    diffusion_model = DiffusionGenerator(sigma=0.2, knot=4, rename_uids=True)
-    diffusion_model.train(df)
-    synth_df = diffusion_model.transform(df, n_series=1)
-    assert synth_df.shape[0] == 10
-    assert synth_df['unique_id'].nunique() == 1
-
-    print('All tests passed!')
-    
