86e3b3557f401806e862c4c5306eb05cc6ee4ddf
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2026-09-08 10:14:23 +0100

    Bump version: 0.2.2 → 0.2.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "metaforecast"
 copyright = "2024–2026, Vitor Cerqueira"
 author = "Vitor Cerqueira"
-release = "0.2.2"
+release = "0.2.3"
 
 # -- General configuration ---------------------------------------------------
 
